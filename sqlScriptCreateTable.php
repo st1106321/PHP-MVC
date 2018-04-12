5aac5db897ddfe133e1be4fb652f909e962668c2
st1106321 <1106321@laurea.fi>
2018-04-12 04:52:24 +0300

    config p2

--- sqlScriptCreateTable.php
+++ sqlScriptCreateTable.php
@@ -1,10 +1,10 @@
 <?php
 
 /* db variables */
-$servername = "localhost"; // example : '127.0.1.1'
-$username = "user2";
-$password = "";
-$dbname = "php-db2";
+$servername = "127.0.0.1:50426";
+$dbname = "localdb";
+$username = "azure";
+$password ="6#vWHD_$";
 
 /* Create connection */
 $conn = new mysqli($servername, $username, $password, $dbname);
